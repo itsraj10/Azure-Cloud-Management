d1f4477273f88d2bed8b78a60f759faca45f99a0
itsraj10 <113260599+itsraj10@users.noreply.github.com>
2024-12-24 09:52:56 +0530

    added files

---
+++ Resources/Automating IIS2 (Scripts).ps1
@@ -0,0 +1,8 @@
+﻿#Import the ServManager module
+Import-Module ServerManager
+
+#Install IIS and all necessary features
+Add-WindowsFeature Web-Server -IncludeAllSubFeature
+
+#create the index.html file with specified content
+Set-Content -Path "C:\inetpub\wwwroot\index.html" -Value "This is VM2"
